e7206b3e927dc359f1dc4f082f9de49b500bc9f6
Raquel Molina <raquelmolinareche@gmail.com>
2022-03-26 15:19:53 +0100

    NTP-P1- Finish imperative version

---
+++ Practica1_TSP/TSP_imperativo/src/PruebaSistema.java
@@ -0,0 +1,64 @@
+import modelo.*;
+
+/**
+ * Clase ProblemaPrueba para probar la funcionalidad de
+ * creacion de objetos de la clase Problema mediante la
+ * lectura de archivo de datos
+ */
+public class PruebaSistema {
+   public static void main(String args[]){
+      // nombre archivo con los datos del problema
+      String nombreArchivo = "./data/berlin52.tsp";
+
+      // Descomentar el modo que se quiera usar y comentar el resto
+
+      //----------------------------------------------------------------------------------------
+      //                    HEURISTICA MONTECARLO
+      //----------------------------------------------------------------------------------------
+
+      // se crea el objeto mediante el metodo factoria
+//      MapaTSP objeto = SolucionadorGenerico.factoria(
+//              nombreArchivo,
+//              ModoVista.GRAFICO,
+//              ModoDistancia.EUCLIDEA,
+//              ModoHeuristica.MC);
+
+      //----------------------------------------------------------------------------------------
+      //                    HEURISTICA VECINO MAS CERCANO
+      //---------------------------------------------------------------------------------------
+
+      // se crea el objeto mediante el metodo factoria
+//      MapaTSP objeto2 = SolucionadorGenerico.factoria(
+//              nombreArchivo,
+//              ModoVista.GRAFICO,
+//              ModoDistancia.EUCLIDEA,
+//              ModoHeuristica.VMC);
+
+      //----------------------------------------------------------------------------------------
+      //                    HEURISTICA 2OPT
+      //----------------------------------------------------------------------------------------
+
+      // se crea el objeto mediante el metodo factoria
+      // Cambiar el Modo2OTP a MC o VMC
+      // según si se quiera generar la primera mejor ruta con MonteCarlo o con el Vecino Más Cercano
+      MapaTSP objeto = SolucionadorGenerico.factoria(
+              nombreArchivo,
+              ModoVista.GRAFICO,
+              ModoDistancia.EUCLIDEA,
+              ModoHeuristica.OPT2,
+              Modo2OPT.MC);
+
+
+
+      //----------------------------------------------------------------------------------------
+      // RESOLVER
+
+      // se llama al metodo de resolucion
+      objeto.resolver();
+
+      // se llama al metodo mostrar, para ver la ruta optima
+      // objeto.mostrarMapa();
+      objeto.mostrarRuta();
+
+   }
+}
---
+++ Practica1_TSP/TSP_imperativo/src/modelo/ModoHeuristica.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public enum ModoHeuristica {
+   MC, VMC, OPT2
+}
