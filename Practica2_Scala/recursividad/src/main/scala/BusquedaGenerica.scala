53e343d08c060dcd771bdfe8da346c7c2ffb6d66
Raquel Molina <raquelmolinareche@gmail.com>
2022-05-21 18:40:38 +0200

    NTP-P2 Refactor Fibonacci method

--- Practica2_Scala/recursividad/src/main/scala/BusquedaGenerica.scala
+++ Practica2_Scala/recursividad/src/main/scala/BusquedaGenerica.scala
@@ -1,5 +1,3 @@
-import scala.::
-
 /**
  * Ejercicio 3: Búsqueda genérica en colecciones ordenadas
  */
@@ -20,119 +18,106 @@ object BusquedaGenerica {
 
   def busquedaASaltosGenerica(buscado: Int, coleccion: List[Int])  : Int = {
 
+    def metodoSaltos(coleccion: List[Int], tam_bloque: Int)  : Int = {
+
+      // Comparar el elemento buscado con el final del primer bloque
+      val primer_bloque = coleccion.take(tam_bloque)
+
+      if (buscado == primer_bloque.last) {
+        // Si el elemento buscado es el ultimo elemento de este bloque
+        tam_bloque
+      }
+      else if (buscado > primer_bloque.last) {
+        // Si el elemento buscado es mayor entonces se salta este bloque
+        val restante = coleccion.takeRight(coleccion.length-tam_bloque)
+        tam_bloque+metodoSaltos(restante, tam_bloque)
+      }
+      else {
+        // Si el elemento buscado es menor al último elemento del bloque entonces
+        // se hace una busqueda lineal en el bloque actual
+        busquedaLineal(primer_bloque)
+      }
+    }
+
+    def busquedaLineal(coleccion: List[Int]) : Int = {
+      for (i <- coleccion.indices) {
+        if (coleccion(i) == buscado) {
+          return i
+        }
+      }
+      -1
+    }
+
+
     // Tamaño de bloque es la raíz cuadrada de la longitud de la colección
     val tam_bloque = math.sqrt(coleccion.length).toInt
 
     // Se van dando saltos por la colección haciendo la búsqueda
-    saltosColeccion(buscado,coleccion,tam_bloque)
-  }
-
-  def saltosColeccion(buscado: Int, coleccion: List[Int], tam_bloque: Int)  : Int = {
-
-    // Comparar el elemento buscado con el final del primer bloque
-    val primer_bloque = coleccion.take(tam_bloque)
-
-    if (buscado == primer_bloque.last) {
-      // Si el elemento buscado es el ultimo elemento de este bloque
-      tam_bloque
-    }
-    else if (buscado > primer_bloque.last) {
-      // Si el elemento buscado es mayor entonces se salta este bloque
-      val restante = coleccion.takeRight(coleccion.length-tam_bloque)
-      tam_bloque+saltosColeccion(buscado, restante,tam_bloque)
-    }
-    else {
-      // Si el elemento buscado es menor al último elemento del bloque entonces
-      // se hace una busqueda lineal en el bloque actual
-      busquedaLineal(buscado, primer_bloque)
-    }
-  }
-
-  def busquedaLineal(buscado: Int, coleccion: List[Int]) : Int = {
-    var pos : Int = -1
-    for (i <- coleccion.indices) {
-      if (coleccion(i) == buscado) {
-        pos = i
-      }
-    }
-    pos
-  }
+    metodoSaltos(coleccion,tam_bloque)
 
 
-
-
-
-  def numeroFibonacciSuperior(n : Int) : Int = {
-    for (i <- 0 to n) {
-      if (fibonacci(i) >= n) {
-        return fibonacci(i)
-      }
-    }
-    -1
-  }
-
-  def anteriorFibonacci(n : Int) : Int = {
-    var anterior = fibonacci(0)
-    for (i <- 0 to n) {
-      if (fibonacci(i) >= n) {
-        return anterior
-      }
-      else {
-        anterior = fibonacci(i)
-      }
-    }
-    anterior
   }
 
   def busquedaMetodoFibonacci(buscado: Int, coleccion: List[Int])  : Int = {
+
+    def metodoFibonnaci(_f0: Int, _f1: Int, _f2: Int, _inicio :Int, _n : Int ) : Int = {
+
+      var f0 = _f0
+      var f1 = _f1
+      var f2 = _f2
+      var inicio = _inicio
+
+      // se determina el valor de indice a considerar, calculando el minimo entre (f0 + inicio)
+      // y el tamaño de la secuencia n (como es un indice sera n-1)
+      val indice = Math.min(f0 + inicio, _n - 1)
+
+      if (buscado > coleccion(indice)) {
+        // si el valor a buscar es mayor, entonces los numeros de Fibonacci a considerar para la siguiente iteracion son
+        // f0 = f1-f0 y f1 = f0 y el nuevo valor de inicio a usar será el tomado por indice.
+        f2  = f1
+        f1 = f0
+        f0 = f2-f0
+        inicio = indice
+      }
+      else if (buscado < coleccion(indice)) {
+        // en caso contrario, si el valor buscado es menor, los numeros de Fibonacci a considerar son f0 = f0-(f1-f0)
+        // y f1 = f1-f0  no cambia el valor de inicio
+        f2  = f0
+        f0 = f0-(f1-f0)
+        f1 = f1-f0
+      }
+      else {
+        // si el valor almacenado en la posicion dada indice por coincide con el valor a buscar, hemos terminado la busqueda
+        return indice
+      }
+
+      if (f2 > 1) {
+        return metodoFibonnaci(f0, f1, f2, inicio, _n)
+      }
+
+      -1
+    }
+
 
     // sea n el numero de elementos en la lista o secuencia de elementos en los que buscar (que deben estar ordenada).
     val n =  coleccion.length
 
     // Se determina el primero numero de Fibonacci superior o igual a n. Sea este numero f2 y los dos anteriores f1 y f0.
-    val f2 = numeroFibonacciSuperior(n)
-    val f1 = anteriorFibonacci(f2)
-    val f0 = anteriorFibonacci(f1)
 
-    println("f2: "+f2)
-    println("f1: "+f1)
-    println("f0: "+f0)
+    var f0 = 0 // Primer número de la sucesión de fibonacci
+    var f1 = 1 // Segundo número de la sucesión de fibonacci
+    var f2 = f0 + f1 // Tercer número de la sucesión de fibonacci
+
+    while (f2 < n) {
+      f0 = f1
+      f1 = f2
+      f2  = f0 + f1
+    }
 
     // Se hace que la variable inicio tenga valor -1
     val inicio = -1
 
-    metodoFibonnaci(buscado,coleccion,f0,f1,inicio)
-  }
-
-  def metodoFibonnaci(buscado : Int, coleccion : List[Int], v_f0 : Int,v_f1 : Int, v_inicio : Int) : Int = {
-    var f0 = v_f0
-    var f1 = v_f1
-    var inicio = v_inicio
-
-    // se determina el valor de indice a considerar, calculando el minimo entre (f0 + inicio)
-    // y el tamaño de la secuencia n.
-    var indice = Math.min(f0+inicio, coleccion.length)
-    println(indice)
-
-    // si el valor almacenado en la posicion dada indice por coincide con el valor a buscar, hemos terminado.
-    if (coleccion(indice) == buscado) {
-      println("ola"+indice)
-      indice
-    }
-    else if (coleccion(indice) > buscado) {
-      // si el valor a buscar es mayor, entonces los numeros de Fibonacci a considerar para la siguiente iteracion son
-      // f0 = f1-f0 y f1 = f0 y el nuevo valor de inicio a usar será el tomado por indice.
-      f0 = f1-f0
-      f1 = f0
-      inicio = indice
-      metodoFibonnaci(buscado,coleccion,f0,f1,inicio)
-    }
-    else {
-      // en caso contrario, los numeros de Fibonacci a considerar son f0 = f0-(f1-f0) y f1 = f1-f0  no cambia el valor de inicio.
-      f0 = f0-(f1-f0)
-      f1 = f1-f0
-      metodoFibonnaci(buscado,coleccion,f0,f1,v_inicio)
-    }
+    metodoFibonnaci(f0,f1,f2,inicio,n)
   }
 
 
