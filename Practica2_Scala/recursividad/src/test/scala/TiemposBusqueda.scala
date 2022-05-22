4f7bcb6e5cd0132b5f944a3a22c65a4409852866
Raquel Molina <raquelmolinareche@gmail.com>
2022-05-22 23:17:04 +0200

    NTP-P2 Add time

--- Practica2_Scala/recursividad/src/test/scala/TiemposBusqueda.scala
+++ Practica2_Scala/recursividad/src/test/scala/TiemposBusqueda.scala
@@ -11,19 +11,35 @@ object TiemposBusqueda extends App{
   ) withWarmer new Warmer.Default
 
   // se fija el valor a buscar y la colección a buscar
-  val coleccion = (1 to 750000).map(i => i).toList
-  val buscado = 350000
+  val coleccion_big = (1 to 750000).map(i => i).toList
+  val buscado_big = 350000
+
+  val coleccion_small = (1 to 100).map(i => i).toList
+  val buscado_small = 20
 
   // se prueba el metodo de busqueda
-  val saltos = standardConfig measure{
-    busquedaASaltosGenerica(buscado,coleccion, mayor)
+  val saltos_big = standardConfig measure{
+    busquedaASaltosGenerica(buscado_big,coleccion_big, mayor)
   }
 
-  val fibonacci = standardConfig measure {
-    busquedaMetodoFibonacci(buscado,coleccion, mayor, menor)
+  val fibonacci_big = standardConfig measure {
+    busquedaMetodoFibonacci(buscado_big,coleccion_big, mayor, menor)
+  }
+
+  val saltos_small = standardConfig measure{
+    busquedaASaltosGenerica(buscado_small,coleccion_small, mayor)
+  }
+
+  val fibonacci_small = standardConfig measure {
+    busquedaMetodoFibonacci(buscado_small,coleccion_small, mayor, menor)
   }
 
   // muestra los tiempos
-  println("Tiempo búsqueda a saltos genérica: " + saltos)
-  println("Tiempo búsqueda método fibonacci: " + fibonacci)
+  println("TIEMPO DE BÚSQUEDA PARA UNA COLECCIÓN GRANDE")
+  println("Tiempo búsqueda a saltos genérica: " + saltos_big)
+  println("Tiempo búsqueda método fibonacci: " + fibonacci_big)
+
+  println("\nTIEMPO DE BÚSQUEDA PARA UNA COLECCIÓN PEQUEÑA")
+  println("Tiempo búsqueda a saltos genérica: " + saltos_small)
+  println("Tiempo búsqueda método fibonacci: " + fibonacci_small)
 }
