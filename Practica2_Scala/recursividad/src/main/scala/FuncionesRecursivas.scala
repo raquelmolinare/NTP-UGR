810756f773f80de53a32fbc6fa6e1330dcdfad7e
Raquel Molina <raquelmolinareche@gmail.com>
2022-05-22 23:23:12 +0200

    NTP-P2 Modify text

--- Practica2_Scala/recursividad/src/main/scala/FuncionesRecursivas.scala
+++ Practica2_Scala/recursividad/src/main/scala/FuncionesRecursivas.scala
@@ -273,7 +273,7 @@ object FuncionesRecursivas {
     println("Número de cambios posibles: "+ listarCambiosPosibles(cantidad,monedas))
 
 
-    println("\n................... Búsqueda genérica ...................")
+    println("\n................... Búsqueda ...................")
 
     val buscado : Int = 55
     val coleccion: List[Int] = List(1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987)
