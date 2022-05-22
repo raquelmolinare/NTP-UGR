58acb3172323c809f49708135b2e0fa73e2555b1
Raquel Molina <raquelmolinareche@gmail.com>
2022-05-22 13:25:41 +0200

    NTP-P2 Add test CambiosDeMonedaCheck

---
+++ Practica2_Scala/recursividad/src/test/scala/CambiosDeMonedaCheck.scala
@@ -0,0 +1,18 @@
+import CambiosDeMoneda.listarCambiosPosibles
+import org.scalacheck.Prop.all
+import org.scalacheck.Properties
+
+object CambiosDeMonedaCheck extends Properties("Cambios De Moneda Check") {
+
+  property("Numero de cambio de monedas") = {
+    val check_cambio1 = listarCambiosPosibles(4, List(1,2)) == 3
+    val check_cambio2 = listarCambiosPosibles(4, List(1,2,3)) == 4
+    val check_cambio3 = listarCambiosPosibles(50, List(10,20,30)) == 5
+    val check_cambio4 = listarCambiosPosibles(200, List(20,20,100)) == 6
+    val check_cambio5 = listarCambiosPosibles(5, List(10,20)) == 0
+    val check_cambio6 = listarCambiosPosibles(5, List()) == 0
+
+    all(check_cambio1,check_cambio2,check_cambio3,check_cambio4,check_cambio5,check_cambio6)
+  }
+
+}
